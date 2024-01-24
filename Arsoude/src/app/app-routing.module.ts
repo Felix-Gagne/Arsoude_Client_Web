08d783d14ee67518826068e82edca6b518e056b1
Andy Ly <2157338@cegepmontpetit.ca>
2024-01-24 16:58:10 -0500

    TEMP_LoginPage

--- Arsoude/src/app/app-routing.module.ts
+++ Arsoude/src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authentification/login/login.component';
 
 const routes : Routes = [
-{path: '', component : HomeComponent}
-
-
-  
-]
+  {path: '', component : LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
