ae4582341aeeaf702855a21db53f73bc7d0f6f8a
Félix Gagné <2070094@cegepmontpetit.ca>
2024-01-25 16:16:27 -0500

    FCT_Login

--- Arsoude/src/app/service/user.service.ts
+++ Arsoude/src/app/service/user.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { RegisterDTO } from '../models/RegisterDTO';
 import { lastValueFrom } from 'rxjs';
+import { RegisterDTO } from '../models/RegisterDTO';
+import { LoginDTO } from '../models/LoginDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,15 @@ export class UserService {
 
   constructor(public http : HttpClient) { }
 
-  async register(service : RegisterDTO)
+  async register(dto : RegisterDTO)
   {
-    let x = await lastValueFrom(this.http.post<RegisterDTO>("http://localhost:5050/api/User/Register", service));
+    let x = await lastValueFrom(this.http.post<RegisterDTO>("http://localhost:5050/api/User/Register", dto));
     console.log(x);
+  }
+
+  async Login(dto : LoginDTO){
+    let x = await lastValueFrom(this.http.post<any>("http://localhost:5050/api/User/Login", dto));
+    console.log(x.token);
+    localStorage.setItem("Token", x.token);
   }
 }
