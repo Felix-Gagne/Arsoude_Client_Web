a110bc41ed7e36f9fb8c605a7b65c8872c954d1d
Guillaume Gobeil-Bouchard <2028873@cegepmontpetit.ca>
2024-02-01 10:17:39 -0500

    Fix: Ajout des environnement nécessaire

--- Arsoude/src/app/service/trail.service.ts
+++ Arsoude/src/app/service/trail.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { TrailDTO } from '../models/TrailDTO';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,10 @@ import { TrailDTO } from '../models/TrailDTO';
 export class TrailService {
 
   constructor(public http : HttpClient, public router : Router) { }
-
+  private baseUrl = environment.apiUrl + 'api/Trail/'
   async CreateTrail(trail : TrailDTO){
     try{
-      let x = await lastValueFrom(this.http.post<any>("http://localhost:5050/api/Trail/CreateTrail", trail));
+      let x = await lastValueFrom(this.http.post<any>(this.baseUrl+"CreateTrail", trail));
       console.log(x);
       this.router.navigate(['/']);
     }
@@ -24,7 +25,7 @@ export class TrailService {
 
   async GetUserTrails(trail : TrailDTO){
     try{
-      let x = await lastValueFrom(this.http.get<any>("http://localhost:5050/api/Trail/GetUserTrails"));
+      let x = await lastValueFrom(this.http.get<any>(this.baseUrl+"GetUserTrails"));
       console.log(x);
     }
     catch(e){
--- Arsoude/src/app/service/user.service.ts
+++ Arsoude/src/app/service/user.service.ts
@@ -5,6 +5,7 @@ import { RegisterDTO } from '../models/RegisterDTO';
 import { LoginDTO } from '../models/LoginDTO';
 import { Router } from '@angular/router';
 import { InfoRegDTO } from '../models/InfoRegDTO';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,12 @@ import { InfoRegDTO } from '../models/InfoRegDTO';
 export class UserService {
 
   constructor(public http : HttpClient, public router : Router) { }
-
+  private baseUrl = environment.apiUrl + 'api/User'
   async register(dto : RegisterDTO)
   {
     try{
       console.log(dto);
-      let x = await lastValueFrom(this.http.post<RegisterDTO>("http://localhost:5050/api/User/Register", dto));
+      let x = await lastValueFrom(this.http.post<RegisterDTO>(this.baseUrl+"/Register", dto));
       console.log(x);
       this.router.navigate(['/infoReg', dto.Username]);
     }
@@ -28,7 +29,7 @@ export class UserService {
 
   async Login(dto : LoginDTO){
     try{
-      let x = await lastValueFrom(this.http.post<any>("http://localhost:5050/api/User/Login", dto));
+      let x = await lastValueFrom(this.http.post<any>(this.baseUrl+"/Login", dto));
       console.log(x.token);
       localStorage.setItem("Token", x.token);
       this.router.navigate(['/']);
@@ -41,7 +42,7 @@ export class UserService {
   async AddAditionnalInfo(dto : InfoRegDTO){
     try{
       console.log(dto);
-      let x = await lastValueFrom(this.http.put<any>("http://localhost:5050/api/User/AddAditionnalInfo", dto));
+      let x = await lastValueFrom(this.http.put<any>(this.baseUrl+"/AddAditionnalInfo", dto));
       console.log(x);
       this.router.navigate(['']);
     }
