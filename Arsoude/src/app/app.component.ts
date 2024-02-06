b43a926ee223ecc543126f1f896e3cb1bdb7ca99
Vlad <2184803@cegepmontpetit.ca>
2024-02-06 14:32:34 -0500

    FCT_FunctionLogout : Ajout du logout

--- Arsoude/src/app/app.component.ts
+++ Arsoude/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from './service/user.service';
+import { faBicycle, faPersonWalking } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Arsoude';
+
+  subMenu: HTMLElement | null = null;
+
+
+  constructor(public userService : UserService) { }
+
+  ngOnInit(): void{
+    this.userService.verifyConnectedUser();
+    this.subMenu = document.getElementById("subMenu");
+
+  }
+
+  toggleMenu(): void {
+    console.log("Function called");
+    if (this.subMenu) {
+      this.subMenu.classList.toggle("open-menu");
+    }
+  }
+
+  logout(): void{
+    if(this.subMenu){
+      this.subMenu.classList.remove("open-menu");
+    }
+    this.userService.Logout();
+  }
+
 }
