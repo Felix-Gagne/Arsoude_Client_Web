e85805f3c8eb5d8b6015c8e7594e0112c948de9a
Félix Gagné <2070094@cegepmontpetit.ca>
2024-02-20 15:48:30 -0500

    FCT_ModifUser

---
+++ Arsoude/src/app/models/ModifUserDTO.ts
@@ -0,0 +1,14 @@
+export class ModifUserDTO{
+    constructor(
+        public lastName? : string,
+        public firstName? : string,
+        public areaCode? : string,
+        public houseNo? : number,
+        public street? : string,
+        public city? : string,
+        public state? : string,
+        public yearOfBirth? : number,
+        public monthOfBirth? : number,
+        public avatarUrl? : string
+    ){}
+}
--- Arsoude/src/app/profile/profile/profile.component.ts
+++ Arsoude/src/app/profile/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModifUserDTO } from 'src/app/models/ModifUserDTO';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,6 +9,53 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
-  
+  user : ModifUserDTO = new ModifUserDTO();
+  isEditorMode : boolean = false;
 
+  firstName? : string;
+  lastName? : string;
+  areaCode? : string;
+  houseNo? : number;
+  street? : string;
+  city? : string;
+  state? : string;
+  yearOfBirth? : number;
+  monthOfBirth? : number;
+
+
+  constructor(public service : UserService){}
+
+  async ngOnInit(){
+    this.user = await this.service.getUserInfo();
+    console.log(this.user)
+  }
+
+  switchMode(){
+    if(this.isEditorMode == false){
+      this.lastName = undefined;
+      this.firstName = undefined;
+      this.areaCode = undefined;
+      this.houseNo = undefined;
+      this.street = undefined;
+      this.city = undefined;
+      this.state = undefined;
+      this.yearOfBirth = undefined;
+      this.monthOfBirth = undefined;
+      this.isEditorMode = true
+    }
+    else{
+      this.isEditorMode = false;
+    }
+  }
+
+  async edit(){
+    let dto = new ModifUserDTO(this.lastName, this.firstName, this.areaCode, this.houseNo, this.street, 
+      this.city, this.state, this.yearOfBirth, this.monthOfBirth);
+
+    await this.service.editUser(dto);
+
+    this.user = await this.service.getUserInfo();
+
+    this.isEditorMode = false;
+  }
 }
