a12ababbe48751c0c6482a13484dd036116a3882
Vlad <2184803@cegepmontpetit.ca>
2024-03-11 13:44:01 -0400

    FCT_OptionResearch : Ajout de l'option du changement de radius

--- Arsoude/src/app/newSearch/new-search-page/new-search-page.component.ts
+++ Arsoude/src/app/newSearch/new-search-page/new-search-page.component.ts
@@ -1,7 +1,10 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { faAngleDown, faBicycle, faPersonWalking } from '@fortawesome/free-solid-svg-icons';
-import { latLng, LatLng, tileLayer, TileLayerOptions } from 'leaflet';
+import * as L from 'leaflet';
+import { latLng, LatLng, tileLayer, TileLayerOptions, Layer } from 'leaflet';
+import { Observable } from 'rxjs/internal/Observable';
+import { Observer } from 'rxjs/internal/types';
 import { FilterDTO } from 'src/app/models/FilterDTO';
 import { TrailDTO } from 'src/app/models/TrailDTO';
 import { TrailType } from 'src/app/models/enum/Type';
@@ -12,8 +15,8 @@ import { TrailService } from 'src/app/service/trail.service';
   templateUrl: './new-search-page.component.html',
   styleUrls: ['./new-search-page.component.css']
 })
-export class NewSearchPageComponent {
-  
+export class NewSearchPageComponent{
+  @ViewChild('mapContainerOptions') mapContainerOptions!: ElementRef;
 
   trails: TrailDTO[] = [];
   selectedType?: string;
@@ -24,18 +27,66 @@ export class NewSearchPageComponent {
   type : TrailType = TrailType.Undefined;
   radius : number = 0;
   minRadius : number = 10;
+  rangeValue: number = 50;
+
 
   emptyList : boolean = false;
-
   //pagination
   pagedTrails: TrailDTO[] = [];
   currentPage: number = 1;
   pageSize: number = 5;
   trailsLength: number = 0;
+  layers: Layer[] = [];
+
+  //Map section 
+  optionsSpec: any = {
+    layers: [{ url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', attribution: 'Open Street Map' }],
+    zoom: 5,
+    center: [46.879966, -121.726909]
+  };
+  zoom = this.optionsSpec.zoom;
+  center = latLng(this.optionsSpec.center);
+
+  tileLayerOptions: TileLayerOptions = {
+    attribution: this.optionsSpec.layers[0].attribution
+  };
+
+  southWest: L.LatLng = L.latLng(84.953827, -176.559498);
+  northEast: L.LatLng = L.latLng(-84.295994, 180.559498);
+  bounds: L.LatLngBounds = L.latLngBounds(this.southWest, this.northEast);
+
+  currentLat: string = "";
+  currentLng: string = "";
+
+
+  options = {
+    layers: [tileLayer(this.optionsSpec.layers[0].url, this.tileLayerOptions)],
+    zoom: this.optionsSpec.zoom,
+    center: latLng(this.optionsSpec.center),
+    minZoom : 2,
+    maxZoom : 140,
+    maxBounds : this.bounds
+  };
+
+  optionsSecondMap = {
+      layers: [L.tileLayer(this.optionsSpec.layers[0].url, { attribution: this.optionsSpec.layers[0].attribution })],
+      zoom: this.optionsSpec.zoom,
+      center: L.latLng(this.optionsSpec.center),
+      minZoom : 2,
+      maxZoom : 140,
+      maxBounds : this.bounds
+  }
+
+  formZoom = this.zoom;
+  zoomLevels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  lat = this.center.lat;
+  lng = this.center.lng;
 
   constructor( private router: Router,private trailService : TrailService, private activedRoute: ActivatedRoute){}
+  
 
   async ngOnInit(){
+    this.openMenu();
     let param = this.activedRoute.snapshot.paramMap.get("keyword")
 
     let dto = new FilterDTO(param?.toString());
@@ -50,54 +101,8 @@ export class NewSearchPageComponent {
       this.trailsLength = this.trails.length;
       this.updatePagedTrail();
     }
-  }
+  } 
 
-  optionsSpec: any = {
-    layers: [{ url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', attribution: 'Open Street Map' }],
-    zoom: 5,
-    center: [46.879966, -121.726909]
-  };
-
-  zoom = this.optionsSpec.zoom;
-  center = latLng(this.optionsSpec.center);
-
-  tileLayerOptions: TileLayerOptions = {
-    attribution: this.optionsSpec.layers[0].attribution
-  };
-
-  options = {
-    layers: [tileLayer(this.optionsSpec.layers[0].url, this.tileLayerOptions)],
-    zoom: this.optionsSpec.zoom,
-    center: latLng(this.optionsSpec.center)
-  };
-
-  formZoom = this.zoom;
-  zoomLevels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  lat = this.center.lat;
-  lng = this.center.lng;
-
-  onCenterChange(center: LatLng) {
-    setTimeout(() => {
-      this.lat = center.lat;
-      this.lng = center.lng;
-    });
-  }
-
-  onZoomChange(zoom: number) {
-    setTimeout(() => {
-      this.formZoom = zoom;
-    });
-  }
-
-  doApply() {
-    this.center = latLng(this.lat, this.lng);
-    this.zoom = this.formZoom;
-  }
-
-  
-
-
- 
 
   updatePagedTrail(){
     const startIndex = (this.currentPage - 1) * this.pageSize;
@@ -154,29 +159,102 @@ export class NewSearchPageComponent {
     }
   }
 
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-       this.loadMoreData();
-    }
+  toggleColor(event: MouseEvent) {
+    const element = event.currentTarget as HTMLElement;
+    const containers = document.querySelectorAll('.containerFirstIcon, .containerSecondIcon');
+    containers.forEach(function(container) {
+        container.classList.remove('backgroundColor');
+    });
+    element.classList.add('backgroundColor');
+  } 
+
+
+
+  
+
+  onCenterChange(center: LatLng) {
+    setTimeout(() => {
+      this.lat = center.lat;
+      this.lng = center.lng;
+    });
   }
 
-  async loadMoreData() {
-    let param = this.activedRoute.snapshot.paramMap.get("keyword");
-  
-    let dto = new FilterDTO(param?.toString());
-  
-    if (param != null) {
-      const moreTrails = await this.trailService.searchTrails(dto);
-      this.trails = this.trails.concat(moreTrails);
-      this.trailsLength = this.trails.length;
-      this.updatePagedTrail();
-    } else {
-      const moreTrails = await this.trailService.allTrails();
-      this.trails = this.trails.concat(moreTrails);
-      this.trailsLength = this.trails.length;
-      this.updatePagedTrail();
-    }
+  onZoomChange(zoom: number) {
+    setTimeout(() => {
+      this.formZoom = zoom;
+    });
   }
+
+  doApply() {
+    this.center = latLng(this.lat, this.lng);
+    this.zoom = this.formZoom;
+  }
+
+  openMenu(): void {
+    // Get the current position using geolocation service
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const newCenter = L.latLng(latitude, longitude);
+
+        this.center = newCenter;
+        this.zoom = 14
+       
+        if (this.mapContainerOptions && this.mapContainerOptions.nativeElement) {
+          
+
+          // Create and add circle layer to the map
+          const circle = L.circle([latitude, longitude], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: this.metersToKilometers(1000)
+          });
+
+          this.layers.push(circle);
+          console.log(this.layers);
+
+        } else {
+          console.error('Leaflet map container element not found.');
+        }
+
+        
+      },
+      (error) => {
+        console.error('Error getting current position:', error);
+      }
+    );
+  }
+
+  centerMap(latitude: number, longitude: number) {
+    this.optionsSecondMap.center = L.latLng(latitude, longitude);
+  }
+
+  getPosition(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        resp => {
+          resolve(resp);
+        },
+        err => {
+          console.error('Error getting current position:', err);
+          reject(err);
+        }
+      );
+    });
+  }
+
+  onRangeChange(event: Event) {
+    // Handle range value change logic here
+    console.log('Range value:', this.rangeValue);
+    var rayon = this.layers[0];
+    console.log("Voici le rayon", rayon)
+  }
+
+   metersToKilometers(radiusInMeters: number): number {
+    return radiusInMeters / 1000; // Convert meters to kilometers
+  }
+
+  
 
 }
