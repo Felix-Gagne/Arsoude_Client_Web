a257be74abb9c2047ce291c37e6862672ff4fb97
Vlad <2184803@cegepmontpetit.ca>
2024-03-13 16:20:54 -0400

    FCT_Markers : Ajout des marker sur la map

--- Arsoude/src/app/newSearch/new-search-page/new-search-page.component.ts
+++ Arsoude/src/app/newSearch/new-search-page/new-search-page.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { faAngleDown, faBicycle, faPersonWalking } from '@fortawesome/free-solid-svg-icons';
@@ -35,14 +36,16 @@ export class NewSearchPageComponent{
   //pagination
   pagedTrails: TrailDTO[] = [];
   currentPage: number = 1;
-  pageSize: number = 5;
+  pageSize: number = 6;
   trailsLength: number = 0;
   layers: Layer[] = [];
+	markers: {trailId: number, marker: L.Marker}[] = [];
+  markersMap : Layer[] = [];
 
   //Map section 
   optionsSpec: any = {
     layers: [{ url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', attribution: 'Open Street Map' }],
-    zoom: 5,
+    zoom: 2,
     center: [46.879966, -121.726909]
   };
   zoom = this.optionsSpec.zoom;
@@ -87,7 +90,6 @@ export class NewSearchPageComponent{
   
 
   async ngOnInit(){
-    this.openMenu();
     let param = this.activedRoute.snapshot.paramMap.get("keyword")
 
     let dto = new FilterDTO(param?.toString());
@@ -102,8 +104,213 @@ export class NewSearchPageComponent{
       this.trailsLength = this.trails.length;
       this.updatePagedTrail();
     }
+
+    //make a foreach loop in trails 
+
+
+    this.addMarkers(this.trails);
     this.renderer.removeClass(document.body, 'menu-open');
-  } 
+  }
+  
+  addMarkers(trails: TrailDTO[]){
+    trails.forEach(trail => {
+      var latitude = trail.startingCoordinates?.latitude;
+      var longitude = trail.startingCoordinates?.longitude;
+
+      const newMarker = L.marker(
+        [latitude!, longitude!],
+        {
+          icon: L.icon({
+            iconSize: [ 25, 41 ],
+            iconAnchor: [ 13, 41 ],
+            iconUrl: 'assets/leaflet/marker-icon.png',
+            iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+            shadowUrl: 'assets/leaflet/marker-shadow.png',
+          }),
+        }
+      );
+
+      
+
+      const popupContent = 
+      `
+      <style>
+        .leaflet-popup{
+          width: 300px !important;
+          border-radius: 45px;
+        }
+
+        .leaflet-popup-content{
+          width: 100% !important;
+          margin: 0;
+          border-radius: 45px;
+
+        }
+
+        .leaflet-popup-content-wrapper{
+          width: 100% !important;
+          padding: 0;
+          border-radius: 15px;
+
+        }
+
+        .popup{
+          display: flex;
+          flex-direction: column;
+          width: 300px;
+          height: 250px;
+          border-radius: 15px;
+          overflow-y: auto; /* Add overflow-y property for vertical scrolling */
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add box shadow for depth */
+          cursor : pointer;
+        }
+
+        .imageContainer2{
+          width: 100%;
+          height: 60%;
+          position: relative;
+      }
+      
+      .imageContainer2>img{
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+          transition: 0.3s;
+      }
+
+      .icon2{
+        position: absolute;
+        margin: 15px;
+        top: 0;
+        right: 0;
+        width: fit-content;
+        height: fit-content;
+        display: flex;
+        align-items: center;
+        background-color: white;
+        width: 40px;
+        height: 40px;
+        justify-content: center;
+        border-radius: 15%;
+        box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.3);
+        border: 1px solid #000;
+    }
+    
+    
+    .icon p{
+        margin: 0;
+        font-size: 22px;
+        width: fit-content;
+    }
+
+        .trailImage2{
+          height: 65%;
+          width: 40%;
+          object-fit: cover;
+        }
+
+        .trailTitleInformation2{
+          margin-top: 10px;
+          display: flex;
+          margin-left: 15px;
+        }
+        
+        .trailTitleInformation2 p{
+            width: 65%;
+            margin: 0;
+            font-size: 16px;
+        }
+        
+        .trailName2{
+            font-weight: 570;
+        }
+        
+        .rating2{
+            display: flex;
+            gap: 2px;
+            height: fit-content;
+        }
+
+        .textLocation2{
+          margin:0 !important;
+          margin-left: 15px !important;
+          margin-top: 5px !important;
+          font-weight: 350;
+          font-size: 16px;
+      }
+      
+      .leaflet-popup-close-button{
+        height: fit-content;
+        width: fit-content;
+        background-color: white !important;
+        margin: 15px;
+        border-radius: 90%;
+        left:0 !important;
+        transform : scale(1.2);
+        transition: 0.3s;
+      }
+
+      .leaflet-popup-close-button:hover{
+        background-color: #f0f0f0 !important;
+      }
+
+      .leaflet-popup-close-button span{
+        font : 24px;
+        color: black;
+      }
+
+
+      
+      
+      </style>
+      
+      <div class="popup" (click)="getDetails(${trail.id})">
+          <div class="imageContainer2">
+            <img src="${trail.imageUrl}" alt="image_trail">
+            <div class="icon2">
+              <p>${trail.type == 0 ? '<i class="fa-solid fa-person-walking"></i>' : '<i class="fa-solid fa-bicycle"></i>'}</p>
+            </div>
+          </div>
+        <div class="trailTitleInformation2">
+            <p class="trailName2">${trail.name}</p>
+            <div class="rating2">
+              <i class="fa-solid fa-star"></i>
+              <p>4,88</p>
+              <p>(500)</p>
+            </div>
+        </div>
+        <p class="textLocation2">${trail.location}</p>
+      </div>`;
+    
+      
+
+      newMarker.on('click', (event: L.LeafletEvent) => {
+        // Display the information of the marker when clicked
+        // For example, you can access the popup content like this:
+        
+      });
+      newMarker.bindPopup(popupContent);
+      this.markers.push({ trailId: trail.id!, marker: newMarker });
+    });
+
+
+    const markerLayers = this.markers.map(markerObj => markerObj.marker);
+    this.markersMap = markerLayers;
+    console.log(this.markers);
+    console.log(this.markersMap);
+    console.log(this.trails);
+  }
+
+  displayTrailMarker(trail: TrailDTO) {
+    
+      for (let i = 0; i < this.markers.length; i++) {
+        if (this.markers[i].trailId === trail.id) {
+          this.markers[i].marker.openPopup();
+        }
+      }
+      // Find the marker associated with the given trail ID
+    
+  }
 
 
   updatePagedTrail(){
@@ -120,6 +327,7 @@ export class NewSearchPageComponent{
 
 
   async Search(){
+    this.markers = [];
     let dto = new FilterDTO();
 
     if(this.type != TrailType.Undefined){
@@ -139,6 +347,7 @@ export class NewSearchPageComponent{
     }
     else{
       this.trails = await this.trailService.searchTrails(dto);
+      this.addMarkers(this.trails);
       this.emptyList = false;
       this.trailsLength = this.trails.length;
       this.updatePagedTrail();
@@ -152,6 +361,7 @@ export class NewSearchPageComponent{
   async getDetails(trailId:number){
     try{
       localStorage.setItem("trailid", trailId.toString());
+      console.log(trailId);
       var x = await this.trailService.getTrailDetails(trailId);
       this.router.navigate(['/details', x.name]);
 
@@ -192,14 +402,19 @@ export class NewSearchPageComponent{
     this.zoom = this.formZoom;
   }
 
+  closeMenu(): void {
+    this.trailOptionsVisible = false;
+    if(this.trailOptionsVisible == false){
+      this.renderer.removeClass(document.body, 'menu-open');
+    }
+  }
+
   openMenu(): void {
-    this.trailOptionsVisible = !this.trailOptionsVisible;
+    
+    this.trailOptionsVisible = true;
     // Get the current position using geolocation service
     if(this.trailOptionsVisible){
       this.renderer.addClass(document.body, 'menu-open');
-    }
-    else{
-      this.renderer.removeClass(document.body, 'menu-open');
     }
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -219,6 +434,7 @@ export class NewSearchPageComponent{
             fillOpacity: 0.5,
             radius: this.metersToKilometers(0)
           });
+
 
           this.layers.push(circle);
           console.log(this.layers);
@@ -281,6 +497,12 @@ export class NewSearchPageComponent{
       } else if(this.rangeValue <= 50 || this.rangeValue >= 50){
         this.zoom = 7;
       }
+      //get the current position using the function getPosition
+      this.getPosition().then(position => {
+        const { latitude, longitude } = position.coords;
+        const newCenter = L.latLng(latitude, longitude);
+        this.center = newCenter;
+      });
       circle.setRadius(newRadius);
   }
 
